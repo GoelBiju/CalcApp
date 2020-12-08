dd3ac98cf4c984b7f6ec71dcf8b7ad8001088794
Goel Biju <goelmolel@hotmail.com>
2020-12-08 16:00:41 +0000

    Add addNumber unit test

--- logic.js
+++ logic.js
@@ -5,4 +5,12 @@ function flipCoin() {
   return Math.random() > 0.5;
 }
 
+function addNumber(a, b) {
+  /**
+   * Return result of variable a added with variable b.
+   */
+  return a + b;
+}
+
 module.exports.flipCoin = flipCoin;
+module.exports.addNumber = addNumber;
--- test/unit.js
+++ test/unit.js
@@ -1,13 +1,12 @@
 let chai = require("chai");
 let logic = require("../logic");
 
-suite("Test coin flip", function() {
-
-  test("Test the coin flips properly", function() {
+suite("Test coin flip", function () {
+  test("Test the coin flips properly", function () {
     // Initialise a counter.
     let counter = 0;
 
-    for (let i=0; i<100; i++) {
+    for (let i = 0; i < 100; i++) {
       // Flip the coin.
       let flip = logic.flipCoin();
 
@@ -21,5 +20,14 @@ suite("Test coin flip", function() {
 
     chai.assert.approximately(50, counter, 10);
   });
+});
 
+suite("Test addition", function () {
+  test("Test that a and b are added correctly", function () {
+    let a = 9;
+    let b = 10;
+    let result = 19;
+
+    chai.assert.equal(logic.addNumber(a, b), result, "Result should be 19");
+  });
 });
