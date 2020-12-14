e700960372b0f6486bfbc310c0ca0b989b95241f
Goel Biju <goelmolel@hotmail.com>
2020-12-14 22:21:26 +0000

    Add /add route prefix

--- test/integration.js
+++ test/integration.js
@@ -23,12 +23,12 @@ suite("Integration tests for coin flip system", function () {
 });
 
 suite("Integration test for adding numbers", function () {
-  test("Test GET /number1/number2", function () {
+  test("Test GET /add/number1/number2", function () {
     let app = server.app;
 
     chai
       .request(app)
-      .get("/1/2")
+      .get("/add/1/2")
       .end(function (error, response) {
         chai.assert.equal(response.status, 200, "Wrong status code");
         chai.assert.equal(response.text, "3", "Result not equal to 3");
